f1ca5bf626731f6a6e96095df704a2d761ca2407
adek312 <adrian.dolata@vp.pl>
2023-12-15 16:24:00 +0100

    Added button that creates new grid with given number of squares

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const container = document.getElementById("container");
 
-
-for(let i = 0; i < 16; i++){   
-    for(let j = 0; j < 16; j++){
-        let gridItems = document.createElement("div");
-        gridItems.className = "gridItems";
-        container.appendChild(gridItems);
+function newGrid(){
+    let size = prompt("enter new grid size (max 100)");
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    for(let i = 0; i < size; i++){   
+        for(let j = 0; j < size; j++){
+            let gridItems = document.createElement("div");
+            gridItems.className = "gridItems";
+            container.appendChild(gridItems);
+        }
+        let lineBreak = document.createElement("div");
+        lineBreak.style.flex = '0 0 100%';
+        container.appendChild(lineBreak);
+    
+        let gridItems = document.querySelectorAll('.gridItems');
+        gridItems.forEach(item => {
+            item.addEventListener('mouseenter', () => {
+            item.style.backgroundColor = 'red';
+            });
+        });
     }
-    let lineBreak = document.createElement("div");
-    lineBreak.style.flex = '0 0 100%';
-    container.appendChild(lineBreak);
 }
-
-
-const gridItems = document.querySelectorAll('.gridItems');
-
-gridItems.forEach(item => {
-    item.addEventListener('mouseenter', () => {
-    item.style.backgroundColor = 'red';
-    });
-
-    item.addEventListener('mouseleave', () => {
-    item.style.backgroundColor = '';
-    });
-});
